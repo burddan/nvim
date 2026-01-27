126e298462ed6bcd6691b27a16ac3da27e25c898
burddan <faithsteam8818@gmail.com>
2026-01-27 01:16:17 -0300

    tab mudado

--- lua/burddan/config.lua
+++ lua/burddan/config.lua
@@ -1,6 +1,9 @@
 vim.cmd("set number")
 vim.opt.fillchars = {eob = " "} -- remove ~
-vim.cmd("set tabstop=1")
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
 
 vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
 vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
