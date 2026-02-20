3a4734fda40fe4eb2c2ad86b9c71196b0bd257a2
burddan <faithsteam8818@gmail.com>
2026-02-20 18:16:06 -0300

    adicionado nvim-cmp nos lsp

--- lua/burddan/plugins/nvim-lspconfig.lua
+++ lua/burddan/plugins/nvim-lspconfig.lua
@@ -1,6 +1,32 @@
 return {
-								"neovim/nvim-lspconfig",
-								config = function()
-																vim.lsp.config("clangd", {})
-								end,
+  "neovim/nvim-lspconfig",
+  config = function()
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    vim.lsp.config("clangd", {
+      capabilities = capabilities,
+    })
+
+    vim.lsp.config("pyright", {
+      capabilities = capabilities,
+    })
+
+    vim.lsp.config("html", {
+      capabilities = capabilities,
+    })
+
+    vim.lsp.config("cssls", {
+      capabilities = capabilities,
+    })
+
+    vim.lsp.config("ts_ls", {
+      capabilities = capabilities,
+    })
+
+    vim.lsp.enable("clangd")
+    vim.lsp.enable("pyright")
+    vim.lsp.enable("html")
+    vim.lsp.enable("cssls")
+    vim.lsp.enable("ts_ls")
+  end,
 }
