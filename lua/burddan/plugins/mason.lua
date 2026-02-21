16fa90a62ea0380655b1aa2a3b9eba69abc82ffe
burddan <faithsteam8818@gmail.com>
2026-02-21 02:49:58 -0300

    ensure installed das lsp

--- lua/burddan/plugins/mason.lua
+++ lua/burddan/plugins/mason.lua
@@ -1,7 +1,12 @@
 return {
-	    "williamboman/mason.nvim",
-		config = function()
-				local mason = require("mason")
-				mason.setup({})
-		end,
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  config = function()
+    local mason = require("mason")
+    mason.setup({})
+    mason_lspconfig.setup({
+      -- clangd fica instalado no sistema
+      ensure_installed = { "pyright", "html", "cssls", "tsserver", "lua_ls" },
+    })
+  end,
 }
