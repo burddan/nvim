6f5120e3d64bb2e5a192ddd179a6528c8d9c3759
burddan <faithsteam8818@gmail.com>
2026-02-26 00:42:14 -0300

    outro tema

--- lua/burddan/config.lua
+++ lua/burddan/config.lua
@@ -8,4 +8,4 @@ vim.opt.expandtab = true
 vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
 vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
 
-vim.cmd("colorscheme kanagawa")
+vim.cmd("colorscheme kanagawa-dragon")
